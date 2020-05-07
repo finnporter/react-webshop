44aa53813a2a04cbb43049b5a6874c3ea57cd94b
Finn Porter <iamfinnporter@gmail.com>
2020-05-07 16:57:56 +0100

    adds router and updates menu-items

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats page</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
       </Switch>      
     </div>
   );
